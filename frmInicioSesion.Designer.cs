18017a2a81266cfb18764ca41ca3df1e1d1dd73f
Alumno <Alumno@PC5>
2024-05-27 14:21:47 -0300

    Agregar archivos de proyecto.

---
+++ frmInicioSesion.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace pryInicioSesionLogs
+{
+    partial class frmInicioSesion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.gbLogs = new System.Windows.Forms.GroupBox();
+            this.gbLogs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Location = new System.Drawing.Point(191, 105);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 30);
+            this.btnIngresar.TabIndex = 0;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(59, 53);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(71, 20);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContraseña.Location = new System.Drawing.Point(30, 79);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(100, 20);
+            this.lblContraseña.TabIndex = 2;
+            this.lblContraseña.Text = "Contraseña:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(136, 53);
+            this.txtUsuario.Multiline = true;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(130, 20);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.Location = new System.Drawing.Point(136, 79);
+            this.txtContraseña.Multiline = true;
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.PasswordChar = '*';
+            this.txtContraseña.Size = new System.Drawing.Size(130, 20);
+            this.txtContraseña.TabIndex = 4;
+            // 
+            // gbLogs
+            // 
+            this.gbLogs.Controls.Add(this.txtContraseña);
+            this.gbLogs.Controls.Add(this.btnIngresar);
+            this.gbLogs.Controls.Add(this.txtUsuario);
+            this.gbLogs.Controls.Add(this.lblUsuario);
+            this.gbLogs.Controls.Add(this.lblContraseña);
+            this.gbLogs.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbLogs.Location = new System.Drawing.Point(29, 30);
+            this.gbLogs.Name = "gbLogs";
+            this.gbLogs.Size = new System.Drawing.Size(324, 170);
+            this.gbLogs.TabIndex = 5;
+            this.gbLogs.TabStop = false;
+            this.gbLogs.Text = "Iniciar Sesión";
+            // 
+            // frmInicioSesion
+            // 
+            this.AcceptButton = this.btnIngresar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 236);
+            this.Controls.Add(this.gbLogs);
+            this.MaximizeBox = false;
+            this.Name = "frmInicioSesion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Iniciar Sesión";
+            this.gbLogs.ResumeLayout(false);
+            this.gbLogs.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.GroupBox gbLogs;
+    }
+}
+
---
+++ frmInicioSesion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace pryInicioSesionLogs
+{
+    public partial class frmInicioSesion : Form
+    {
+        private clsUsuario Usuario; // Declaro la variable 'usuario'
+
+        public frmInicioSesion()
+        {
+            InitializeComponent();
+            Usuario = new clsUsuario(); // Instancio 'clsUsuario'
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            string nombreUsuario = txtUsuario.Text; // Asigno el valor de 'txtUsuario' a una variable
+            string claveUsuario = txtContraseña.Text; // Asigno el valor de 'txtContraseña' a una variable
+
+            Usuario.ValidarUsuario(nombreUsuario, claveUsuario); // Llamo al método 'ValidarUsuario'
+
+            if (Usuario.estadoConexion == "Usuario EXISTE") // Verifico el estado de conexión
+            {
+                MessageBox.Show("Inicio de sesión exitoso");
+
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Usuario o clave incorrecta"); // Mensaje de error
+            }
+        }
+    }
+ 
+}
